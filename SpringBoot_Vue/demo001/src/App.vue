0ddc5e7e8a0772ca4a3a7153a905358e17445e62
Qzc <qzc15995917872@outlook.com>
2022-09-28 23:33:55 +0800

    Demo

---
+++ SpringBoot_Vue/demo001/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Header />
+  </div>
+  <router-view />
+</template>
+
+<script>
+import Header from "@/components/Header";
+export default {
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  methods: {},
+  components: { Header },
+};
+</script>
+
+<style scoped></style>
---
+++ SpringBoot_Vue/demo001/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="top">后台管理系统</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  methods: {},
+  components: {},
+};
+</script>
+
+<style scoped>
+.top {
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+  border-bottom: 1px solid black;
+  display: flex;
+}
+</style>
